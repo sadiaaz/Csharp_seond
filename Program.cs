5fad20c49c21efe7f39feb1ad647afa1f16368c7
sadiaaz <sadiasaad1311@gmail.com>
2026-02-12 23:28:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//// namespae Logial ontaniner
+//using System.ComponentModel;
+
+//Syntax
+
+using System;
+
+namespace HelloWorld { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    } }
